299bc6a6d39a5acb9654d7f0e4e85b2f1a0ca2ce
Ritesh Gadre <61073916+RiteshGadre@users.noreply.github.com>
2022-11-14 20:50:00 +0530

    Add files via upload

---
+++ loops.js
@@ -0,0 +1,16 @@
+// For loop
+for(let i=0; i<5; i++) console.log(i);
+
+// While loop
+let x= 3;
+while(x){
+    console.log(x);
+    x--;
+}
+
+// Do while
+let ele= 3;
+do{
+    console.log(ele*3);
+    ele--;
+}while(ele>0)
